f99ff20258a44ddad3e56dc24acb51be03c02bf0
munivendor <156255275+munivendor@users.noreply.github.com>
2024-01-11 22:59:00 -0500

    added reactive form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule],
+  imports: [ ReactiveFormsModule, RouterModule, RouterLink, CommonModule],
   templateUrl:'./signup.component.html' ,
   styleUrls:['./signup.component.css']  
 })
 
-export class SignupComponent {
- 
+export class SignupComponent implements OnInit {
+  loginForm!: FormGroup;
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email])
+    });
+  }
+  onSubmit() {
+    // Handle form submission here
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Additional logic to authenticate user or 
+      // perform other actions
+      this.loginForm.controls["email"].value
+    }
+  }
 
-  
 }
