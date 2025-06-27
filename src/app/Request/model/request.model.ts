d4cc75a7eee66fbb0154a35778840e5cf51b0f4b
Betty Chen <betty.chen@teliapp.com>
2025-06-27 09:24:52 -0400

    removed hardcoded organization ID for request/response workflow, added authorizing official

--- src/app/Request/model/request.model.ts
+++ src/app/Request/model/request.model.ts
@@ -4,28 +4,29 @@ import { RequestStatus } from "./requeststatus.model"
 import { RequestType } from "./requesttype.model"
 
 export interface DecisionMakerSelection {
-    decisionMakerId: number;
-    decisionMakerNumber?: number;
-  }
+  decisionMakerId: number;
+  decisionMakerNumber?: number;
+}
 
-export class Request
-    {
-        requestId!: number
-        requestName!: string
-        requestTypeId!: number
-        categoryId!: number
-        publishDate!: Date
-        publishTime!:Time
-        openDate!: Date
-        openTime!: Time
-        contractStart!: Date
-        contractEnd!: Date    
-        decisionMakerSelections?: DecisionMakerSelection[] 
-        category!: Category;
-        RequestCancelNote!: string | null;
-        requestCancelReasonId!: number;
-        requestStatus!: RequestStatus;
-        requestStatusId!: number;
-        requestType!: RequestType;
-        sourceRequestId?: number;
+export class Request {
+  requestId!: null | number
+  requestName!: string
+  requestTypeId!: null | number
+  categoryId!: null | number
+  publishDate!: null | Date
+  publishTime!: null | Time
+  openDate!: null | Date
+  openTime!: null | Time
+  contractStart!: null | Date
+  contractEnd!: null | Date
+  decisionMakerSelections?: DecisionMakerSelection[]
+  category!: null | Category;
+  requestCancelNote!: string | null;
+  requestCancelReasonId!: null| number;
+  requestStatus!: null | RequestStatus;
+  requestStatusId!: null | number;
+  requestType!: null | RequestType;
+  sourceRequestId?: null | number;
+  authorizingOfficialId!: null | number;
+  organizationId!: number
 }
