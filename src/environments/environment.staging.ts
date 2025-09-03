14f83d1bf5c0ae55b8a88246b8a1327b6f2c6f08
betty <betty.chen@teliapp.com>
2025-09-03 14:04:41 -0400

    additions for staging build

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl:
-    'https://munivendor-test-fjgjd6g9ezhycrbs.westus2-01.azurewebsites.net/',
+    'https://munivendor-test-fjgjd6g9ezhycrbs.westus2-01.azurewebsites.net/api/',
   disableAuthGuard: false,
 };
